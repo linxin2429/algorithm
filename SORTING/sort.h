3f69e561d76ca46358fc4b39b537b36ab63cb6d1
xldeng_global <xldeng@gmail.com>
2021-01-05 20:52:11 +0800

    merge sort done

--- SORTING/sort.h
+++ SORTING/sort.h
@@ -7,6 +7,8 @@
 #include <type_traits>
 #include <string>
 #include <vector>
+#include <cmath>
+#include <algorithm>
 #include "../FUNDAMENTALS/random.h"
 #include "../utils/utils.h"
 
@@ -74,6 +76,8 @@ void insertionSortX(Itr begin, Itr end) {
 template<typename Itr>
 void shellSort(Itr begin, Itr end) {
     int len = end - begin;
+    if (len == 0)
+        return;
     int h = 1;
     while (h < len / 3)
         h = 3 * h + 1;
@@ -81,13 +85,184 @@ void shellSort(Itr begin, Itr end) {
         for (int i = h; i < len; ++i) {
             auto value = *(begin + i);
             auto j = begin + i;
-            for (; j >= begin + h && value < *(j-h); j -= h)
+            for (; j >= begin + h && value < *(j - h); j -= h)
                 *j = std::move(*(j - h));
             *j = std::move(value);
         }
         h /= 3;
     }
 }
+
+// 用函数对象实现
+template<typename Itr>
+class _MergeSort {
+private:
+    using value_type = typename iterator_traits<Itr>::value_type;
+    vector<value_type> aux;
+
+    void _merge(Itr begin, Itr mid, Itr end) {
+        auto i = begin, j = mid;// [begin,mid),[mid,end)
+        for (int k = 0; k < end - begin; k++) {
+            if (i >= mid)
+                aux[k] = std::move(*(j++));
+            else if (j >= end)
+                aux[k] = std::move(*(i++));
+            else if (*i < *j)
+                aux[k] = std::move(*(i++));
+            else
+                aux[k] = std::move(*(j++));
+        }
+        Itr itr = begin;
+        for (int k = 0; k < (end - begin); k++)
+            *(itr++) = aux[k];
+    }
+
+    void _sort(Itr begin, Itr end) {
+        if (end - 1 <= begin)
+            return;
+        Itr mid = begin + ((end - begin) >> 1);
+        _sort(begin, mid);
+        _sort(mid, end);
+        _merge(begin, mid, end);
+    }
+
+    void mergeSort(Itr begin, Itr end) {
+        if (begin == end)
+            return;
+        _sort(begin, end);
+    }
+
+public:
+    _MergeSort(Itr begin, Itr end) : aux(begin, end) {
+        mergeSort(begin, end);
+    }
+};
+
+
+template<typename Itr>
+void mergeSort(Itr begin, Itr end) {
+    _MergeSort<Itr>(begin, end);
+}
+
+/*
+ * 改进
+ * 2.2.2.1 对小规模数组用插入排序 设置CUTOFF
+ * 2.2.2.2 测试数组是否已经有序
+ * 2.2.2.3 不将元素复制到辅助数组
+ */
+template<typename Itr>
+class _MergeSortX {
+private:
+    using value_type = typename iterator_traits<Itr>::value_type;
+    vector<value_type> aux;
+    const int CUTOFF = 15;
+
+    void _merge(Itr srcBeg, Itr srcMid, Itr srcEnd, Itr destBeg, Itr destEnd) {
+        // src 已经有序，转移到dest
+        auto i = srcBeg, j = srcMid;// [begin,mid),[mid,end)
+        auto dest = destBeg;
+        for (int k = 0; k < srcEnd - srcBeg; k++) {
+            //  ++优先级高于*
+            if (i >= srcMid)
+                *dest++ = std::move(*j++);
+            else if (j >= srcEnd)
+                *dest++ = std::move(*i++);
+            else if (*i < *j)
+                *dest++ = std::move(*i++);
+            else
+                *dest++ = std::move(*j++);
+        }
+    }
+
+    // 给dest排序
+    void _sort(Itr srcBeg, Itr srcEnd, Itr destBeg, Itr destEnd) {
+        if ((srcEnd - srcBeg) < CUTOFF) {
+            insertionSortX(destBeg, destEnd);
+            return;
+        }
+        Itr srcMid = srcBeg + ((srcEnd - srcBeg) >> 1);
+        Itr destMid = destBeg + ((destEnd - destBeg) >> 1);
+
+        // 对src排序
+        _sort(destBeg, destMid, srcBeg, srcMid);
+        _sort(destMid, destEnd, srcMid, srcEnd);
+
+        // sort 之后检查[srcBeg,srcEnd) 是否整体有序
+        if (*(srcMid - 1) < *srcMid) {
+            std::copy(srcBeg, srcEnd, destBeg);
+            return;
+        }
+        // src是有序的，merge之后dest也有序，避免了一次复制
+        _merge(srcBeg, srcMid, srcEnd, destBeg, destEnd);
+    }
+
+    void mergeSort(Itr begin, Itr end) {
+        if (begin == end)
+            return;
+        _sort(aux.begin(), aux.end(), begin, end);
+    }
+
+public:
+    _MergeSortX(Itr begin, Itr end) : aux(begin, end) {
+        mergeSort(begin, end);
+    }
+};
+
+template<typename Itr>
+void mergeSortX(Itr begin, Itr end) {
+    _MergeSortX<Itr>(begin, end);
+}
+
+// 自底向上归并
+template<typename Itr>
+class _MergeBU {
+private:
+    using value_type = typename iterator_traits<Itr>::value_type;
+    vector<value_type> aux;
+
+    void mergeSort(Itr beg, Itr end) {
+        int N = end - beg;
+        for (int sz = 1; sz < N; sz += sz) {
+            for (int i = 0; i < N - sz; i += (sz + sz)) {
+                merge(beg + i, beg +i+ sz, std::min(beg +i+ 2 * sz, end));
+            }
+        }
+    }
+
+    void merge(Itr beg, Itr mid, Itr end) {
+        Itr i = beg, j = mid;
+        for (int k = 0; k < (end - beg); ++k) {
+            if (i >= mid)
+                aux[k] = std::move(*j++);
+            else if (j >= end)
+                aux[k] = std::move(*i++);
+            else if (*i < *j)
+                aux[k] = std::move(*i++);
+            else
+                aux[k] = std::move(*j++);
+        }
+        Itr itr = beg;
+        for (int k = 0; k < (end - beg); ++k)
+            *itr++ = aux[k];
+    }
+
+public:
+    _MergeBU(Itr beg, Itr end) : aux(beg, end) {
+        mergeSort(beg, end);
+    }
+
+};
+
+template<typename Itr>
+void mergeBUSort(Itr beg, Itr end) {
+    _MergeBU<Itr>(beg, end);
+}
+
+/*
+ * #############
+ *  以下为辅助函数
+ * #############
+ */
 
 template<typename Type>
 bool isSorted(vector<Type> &v) {
@@ -110,6 +285,12 @@ double testSort(const string &s, vector<double> &v) {
         insertionSortX(beg, end);
     else if ("ShellSort" == s)
         shellSort(beg, end);
+    else if ("MergeSort" == s)
+        mergeSort(beg, end);
+    else if ("MergeSortX" == s)
+        mergeSortX(beg, end);
+    else if ("MergeBUSort" == s)
+        mergeBUSort(beg, end);
 
     return stopwatch.elapsedTime();
 }
@@ -151,11 +332,17 @@ void testSorting(int N, int T) {
     double t2 = testRandomInput(testSort, "InsertionSort", N, T);
     double t3 = testRandomInput(testSort, "InsertionSortX", N, T);
     double t4 = testRandomInput(testSort, "ShellSort", N, T);
+    double t5 = testRandomInput(testSort, "MergeSort", N, T);
+    double t6 = testRandomInput(testSort, "MergeSortX", N, T);
+    double t7 = testRandomInput(testSort, "MergeBUSort", N, T);
 
     cout << "##########\n" << "the speed ratio :  \n";
     cout << "InsertionSort / SelectionSort = " << t1 / t2 << "\n";
     cout << "InsertionSortX / SelectionSort = " << t1 / t3 << "\n";
-    cout << "ShellSortX / SelectionSort = " << t1 / t4 << "\n" << endl;
+    cout << "ShellSort / SelectionSort = " << t1 / t4 << "\n";
+    cout << "MergeSort / SelectionSort = " << t1 / t5 << "\n";
+    cout << "MergeSortX / SelectionSort = " << t1 / t6 << "\n";
+    cout << "MergeBUSort / SelectionSort = " << t1 / t7 << "\n" << endl;
 }
 
 #endif //ALGORITHM_SORT_H
